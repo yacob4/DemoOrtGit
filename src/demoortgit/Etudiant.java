12e3ad41fc2d948482c17ed8b3980ab3a51e8365
TRABELS <TRABELS@DESKTOP-2UFSP7R>
2022-02-08 09:24:53 +0100

    creation du projet demo ort git

---
+++ src/demoortgit/Etudiant.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demoortgit;
+
+/**
+ *
+ * @author TRABELS
+ */
+public class Etudiant 
+{
+    private int id;
+    private String nom;
+    
+    
+}
